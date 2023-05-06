df43e38805cff48584faa9ee386a8efe122f29e4
Soliat81 <alexandre.clermont@free.fr>
2023-05-06 18:53:17 +0100

    Preparation for deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 const path= require('path');
 const public = path.join(__dirname,'public');
 app.use(express.static(public));
@@ -22,4 +24,4 @@ app.set('view engine', 'mustache');
 const router = require('./routes/routeList');
 app.use("/",router);
 
-app.listen(3000, () => {console.log("Server started on local host 3000")});
+app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)});
